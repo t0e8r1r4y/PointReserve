6d6c021824d6538cc230a9f35c8dba675597cc5d
Terry <terryakishin0814@gmail.com>
2022-11-12 01:03:59 +0900

    factory method를 적용하여 테스로직 일부 정리

--- src/test/java/com/pointreserve/reserves/eventdetail/application/service/EventDetailServiceTest.java
+++ src/test/java/com/pointreserve/reserves/eventdetail/application/service/EventDetailServiceTest.java
@@ -6,6 +6,7 @@ import com.pointreserve.reserves.eventdetail.ui.dto.EventDetailCreate;
 import com.pointreserve.reserves.eventdetail.ui.dto.EventDetailResponse;
 import com.pointreserve.reserves.eventdetail.ui.dto.EventDetailSearch;
 import com.pointreserve.reserves.eventreserves.domain.EventReserves;
+import com.pointreserve.reserves.eventreserves.domain.ReservesStatus;
 import com.pointreserve.reserves.eventreserves.infra.EventReservesRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -40,20 +41,48 @@ class EventDetailServiceTest {
         eventDetailRepository.deleteAll();
     }
 
+    private EventDetailCreate createEventDetailCreteFactory(Long memberId, ReservesStatus status, int amount, String eventId, LocalDateTime time){
+        return EventDetailCreate.builder()
+                .membershipId(memberId)
+                .status(status)
+                .amount(amount)
+                .eventId(eventId)
+                .effectiveData(time)
+                .expiryDate(time.plusYears(1))
+                .build();
+    }
+
+    private EventReserves createEventReservesFactory(Long memberId, int amount, ReservesStatus status){
+       return EventReserves.builder().memberId(memberId).amount(amount).status(status).build();
+    }
+
+    private List<EventDetail> makeEvetDetailForRedeem(){
+        List<EventDetail> eventDetailList = new ArrayList<>();
+
+        Long memberId = 1L;
+
+        EventReserves firstSaveUpEvent = eventReservesRepository.save(createEventReservesFactory(memberId,10,SAVEUP));
+        eventDetailList.add(new EventDetailCreate(firstSaveUpEvent).toEntity());
+
+        EventReserves secondSaveUpEvent = eventReservesRepository.save(createEventReservesFactory(memberId,20,SAVEUP));
+        eventDetailList.add(new EventDetailCreate(secondSaveUpEvent).toEntity());
+
+        EventReserves thirdSaveUpEvent = eventReservesRepository.save(createEventReservesFactory(memberId,30,SAVEUP));
+        eventDetailList.add(new EventDetailCreate(thirdSaveUpEvent).toEntity());
+
+        EventReserves forthSaveUpEvent = eventReservesRepository.save(createEventReservesFactory(memberId,40,SAVEUP));
+        eventDetailList.add(new EventDetailCreate(forthSaveUpEvent).toEntity());
+
+        return eventDetailList;
+    }
+
     @Test
     @DisplayName("적립금 적립")
     void saveUpReservesTest() {
         // given
         Long memberId = 1L;
         LocalDateTime testTime = LocalDateTime.of(1982, 7, 13, 14, 25, 00);
-        EventDetailCreate eventDetailCreate = EventDetailCreate.builder()
-                .membershipId(memberId)
-                .status(SAVEUP)
-                .amount(40)
-                .eventId("e-1")
-                .effectiveData(testTime)
-                .expiryDate(testTime.plusYears(1))
-                .build();
+        EventDetailCreate eventDetailCreate = createEventDetailCreteFactory(memberId, SAVEUP, 40, "e-1", testTime);
 
         // when
         String id = eventDetailService.saveUpReserves(eventDetailCreate);
@@ -74,66 +103,24 @@ class EventDetailServiceTest {
     void cancleSavedUpBefore() {
         // given
         Long memberId = 1L;
-        List<EventDetail> given = eventDetailRepository.saveAll(makeEvetDetailForRedeem()); // 기존 적립
+        eventDetailRepository.saveAll(makeEvetDetailForRedeem()); // 기존 적립
         int sumAmountBefore =  eventDetailRepository.sumAmount(memberId);
-        List<EventDetail> beforeList = eventDetailRepository.findAll();
-        System.out.println("before list ==============================");
-        for( EventDetail e : beforeList) {
-            System.out.println(e.getId() + " " + e.getStatus() + " " + e.getAmount() + " " + e.getSignUpId() + " " + e.getCancelId() + " " + e.getEventId());
-        }
-        EventReserves cancelSaveUp = eventReservesRepository.save(EventReserves.builder()
-                .memberId(memberId)
-                .status(CANCLE_SAVEUP)
-                .build());
-
+        EventReserves cancelSaveUp = eventReservesRepository.save(createEventReservesFactory(memberId, 0, CANCLE_SAVEUP));
         EventDetailCreate eventDetailCreate2 = new EventDetailCreate(cancelSaveUp);
         // when
         eventDetailService.cancleSavedUpBefore("e-1", eventDetailCreate2);
         int sumAmountAfter =  eventDetailRepository.sumAmount(memberId);
         // then
-
-        List<EventDetail> afterList = eventDetailRepository.findAll();
-        System.out.println("after list ==============================");
-        for( EventDetail e : afterList) {
-            System.out.println(e.getId() + " " + e.getStatus() + " " + e.getAmount() + " " + e.getSignUpId() + " " + e.getCancelId() + " " + e.getEventId());
-        }
-
         Assertions.assertEquals(sumAmountBefore-10, sumAmountAfter);
     }
-
-    private List<EventDetail> makeEvetDetailForRedeem(){
-        List<EventDetail> eventDetailList = new ArrayList<>();
-
-        Long memberId = 1L;
-        LocalDateTime testTime = LocalDateTime.of(1982, 7, 13, 14, 25, 00);
-
-        EventReserves firstSaveUpEvent = eventReservesRepository.save(EventReserves.builder().memberId(memberId).amount(10).status(SAVEUP).build());
-        eventDetailList.add(new EventDetailCreate(firstSaveUpEvent).toEntity());
-
-        EventReserves secondSaveUpEvent = eventReservesRepository.save(EventReserves.builder().memberId(memberId).amount(20).status(SAVEUP).build());
-        eventDetailList.add(new EventDetailCreate(secondSaveUpEvent).toEntity());
-
-        EventReserves thirdSaveUpEvent = eventReservesRepository.save(EventReserves.builder().memberId(memberId).amount(30).status(SAVEUP).build());
-        eventDetailList.add(new EventDetailCreate(thirdSaveUpEvent).toEntity());
-
-        EventReserves forthSaveUpEvent = eventReservesRepository.save(EventReserves.builder().memberId(memberId).amount(40).status(SAVEUP).build());
-        eventDetailList.add(new EventDetailCreate(forthSaveUpEvent).toEntity());
-
-        return eventDetailList;
-    }
-
 
     @Test
     @DisplayName("적립금 사용 테스트입니다.")
     void redeemReservesTest() {
         // given
         Long memberId = 1L;
-        List<EventDetail> given = eventDetailRepository.saveAll(makeEvetDetailForRedeem()); // 기존 적립
-        EventReserves eventResponse = eventReservesRepository.save(EventReserves.builder()
-                .memberId(memberId)
-                .amount(50)
-                .status(REDEEM)
-                .build());
+        eventDetailRepository.saveAll(makeEvetDetailForRedeem()); // 기존 적립
+        EventReserves eventResponse = eventReservesRepository.save(createEventReservesFactory(memberId, 50, REDEEM));
         EventDetailCreate eventDetailCreate = new EventDetailCreate(eventResponse);
         int sumAmountBefore =  eventDetailRepository.sumAmount(memberId);
         // when
@@ -142,11 +129,7 @@ class EventDetailServiceTest {
         int sumAmountAfter = eventDetailRepository.sumAmount(memberId);
         Assertions.assertEquals((sumAmountBefore-50), sumAmountAfter);
         // one more given
-        EventReserves eventResponse2 = eventReservesRepository.save(EventReserves.builder()
-                .memberId(memberId)
-                .amount(30)
-                .status(REDEEM)
-                .build());
+        EventReserves eventResponse2 = eventReservesRepository.save(createEventReservesFactory(memberId, 30, REDEEM));
         EventDetailCreate eventDetailCreate2 = new EventDetailCreate(eventResponse2);
         // when
         eventDetailService.redeemReserves(eventDetailCreate2);
@@ -160,22 +143,13 @@ class EventDetailServiceTest {
     void cancelRedeemedBeforeTest() {
         // given
         Long memberId = 1L;
-        List<EventDetail> given = eventDetailRepository.saveAll(makeEvetDetailForRedeem()); // 기존 적립
-        EventReserves eventResponse = eventReservesRepository.save(EventReserves.builder()
-                .memberId(memberId)
-                .amount(50)
-                .status(REDEEM)
-                .build());
+        eventDetailRepository.saveAll(makeEvetDetailForRedeem()); // 기존 적립
+        EventReserves eventResponse = eventReservesRepository.save( createEventReservesFactory(memberId, 50, REDEEM) );
         EventDetailCreate eventDetailCreate = new EventDetailCreate(eventResponse);
         int sumAmountBefore =  eventDetailRepository.sumAmount(memberId);
         // when
         eventDetailService.redeemReserves(eventDetailCreate);
-
-        EventReserves cancelRedeem = eventReservesRepository.save(EventReserves.builder()
-                .memberId(memberId)
-                .status(CANCLE_REDEEM)
-                .build());
-
+        EventReserves cancelRedeem = eventReservesRepository.save(createEventReservesFactory(memberId,0, CANCLE_REDEEM));
         EventDetailCreate eventDetailCreate2 = new EventDetailCreate(cancelRedeem);
         // when
         eventDetailService.cancelRedeemedBefore("e-5", eventDetailCreate2);
@@ -190,7 +164,7 @@ class EventDetailServiceTest {
         // given
         Long memberId = 1L;
         for(int i = 0; i < 30; i++) {
-            EventReserves e = eventReservesRepository.save(EventReserves.builder().memberId(memberId).amount(i+1).status(SAVEUP).build());
+            EventReserves e = eventReservesRepository.save(createEventReservesFactory(memberId,(i+1),SAVEUP));
             eventDetailRepository.save(new EventDetailCreate(e).toEntity());
         }
         // when
@@ -202,4 +176,5 @@ class EventDetailServiceTest {
         // then
         Assertions.assertEquals(response.size(), 10);
     }
+
 }
