d913f0680e14bb1b6ebb4d8365a19ed359d5a3bb
Terry <terryakishin0814@gmail.com>
2022-11-27 22:01:02 +0900

    stream 코드 개선

--- src/main/java/com/pointreserve/reserves/accumulationpoint/ui/controller/AccumulatedPointController.java
+++ src/main/java/com/pointreserve/reserves/accumulationpoint/ui/controller/AccumulatedPointController.java
@@ -29,59 +29,59 @@ public class AccumulatedPointController {
 
         Bucket bucket = trafficPlanService.resolveBucket(request);
         ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
-        HttpHeaders httpHeaders = new HttpHeaders();
 
         if(probe.isConsumed()){
             AccumulatedPointResponse response = accumulatedPointService.createAccumulatedPoint(AccumulatedPointCreate.builder()
                                                                         .memberId(memberId).build());
-            httpHeaders.set("X-RateLimit-Remaining", String.valueOf(probe.getRemainingTokens())); // 윈도 내 남은 처리 요청수
-            httpHeaders.set("X-RateLimit-Limit", String.valueOf(bucket.getAvailableTokens())); // 매 윈도우 마다 클라이언트가 전송할 수 있는 요청의 수
-            return ResponseEntity.status(HttpStatus.CREATED).headers(httpHeaders).body(response);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                                 .headers(makeHeaders(bucket,probe))
+                                 .body(response);
         }
 
-        long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
-        httpHeaders.set("X-RateLimit-Limit", String.valueOf(bucket.getAvailableTokens())); // 매 윈도우 마다 클라이언트가 전송할 수 있는 요청의 수
-        httpHeaders.set("X-Ratelimit-Retry-After", String.valueOf(waitForRefill)); // 한도 제한에 걸리지 않으려면 몇 초 뒤에 요청을 다시 보내야 하는지?
-
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).headers(httpHeaders).build();
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                             .headers(makeHeaders(bucket,probe))
+                             .build();
     }
 
     @DeleteMapping("/reserves/delete/{memberId}")
     public ResponseEntity<AccumulatedPointResponse> deleteAccount(@PathVariable Long memberId, HttpServletRequest request) {
         Bucket bucket = trafficPlanService.resolveBucket(request);
         ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
-        HttpHeaders httpHeaders = new HttpHeaders();
 
         if(probe.isConsumed()) {
-            httpHeaders.set("X-RateLimit-Remaining", String.valueOf(probe.getRemainingTokens())); // 윈도 내 남은 처리 요청수
-            httpHeaders.set("X-RateLimit-Limit", String.valueOf(bucket.getAvailableTokens())); // 매 윈도우 마다 클라이언트가 전송할 수 있는 요청의 수
-            return ResponseEntity.status(HttpStatus.OK).headers(httpHeaders).body(accumulatedPointService.deleteAccumulatedPoint(memberId));
+            return ResponseEntity.status(HttpStatus.OK)
+                                 .headers(makeHeaders(bucket,probe))
+                                 .body(accumulatedPointService.deleteAccumulatedPoint(memberId));
         }
 
-        long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
-        httpHeaders.set("X-RateLimit-Limit", String.valueOf(bucket.getAvailableTokens())); // 매 윈도우 마다 클라이언트가 전송할 수 있는 요청의 수
-        httpHeaders.set("X-Ratelimit-Retry-After", String.valueOf(waitForRefill)); // 한도 제한에 걸리지 않으려면 몇 초 뒤에 요청을 다시 보내야 하는지?
-
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).headers(httpHeaders).build();
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                             .headers(makeHeaders(bucket,probe))
+                             .build();
     }
 
     @GetMapping("/reserves/get/{memberId}")
     public ResponseEntity<AccumulatedPointResponse> getAccount(@PathVariable Long memberId, HttpServletRequest request) {
         Bucket bucket = trafficPlanService.resolveBucket(request);
         ConsumptionProbe probe = bucket.tryConsumeAndReturnRemaining(1);
-        HttpHeaders httpHeaders = new HttpHeaders();
 
         if(probe.isConsumed()){
-            httpHeaders.set("X-RateLimit-Remaining", String.valueOf(probe.getRemainingTokens()));
-            httpHeaders.set("X-RateLimit-Limit", String.valueOf(bucket.getAvailableTokens()));
-            return ResponseEntity.status(HttpStatus.OK).headers(httpHeaders).body(accumulatedPointService.getAccumulatedPoint(memberId));
+            return ResponseEntity.status(HttpStatus.OK)
+                                 .headers(makeHeaders(bucket,probe))
+                                 .body( accumulatedPointService.getAccumulatedPoint(memberId) );
         }
 
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS)
+                             .headers(makeHeaders(bucket,probe))
+                             .build();
+    }
+
+    private HttpHeaders makeHeaders(Bucket bucket, ConsumptionProbe probe) {
+        HttpHeaders httpHeaders = new HttpHeaders();
         long waitForRefill = probe.getNanosToWaitForRefill() / 1_000_000_000;
+        httpHeaders.set("X-RateLimit-Remaining", String.valueOf(probe.getRemainingTokens()));
         httpHeaders.set("X-RateLimit-Limit", String.valueOf(bucket.getAvailableTokens()));
         httpHeaders.set("X-Ratelimit-Retry-After", String.valueOf(waitForRefill));
-
-        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).headers(httpHeaders).build();
+        return httpHeaders;
     }
 
 }
